fa69ae0b378746ee3e52c7bbd0a0ca88c6d38a7f
sebastian.huanca@alumnos.ucn.cl <sebastian.huanca@alumnos.ucn.cl>
2024-05-04 18:41:34 -0400

    Agregar archivos de proyecto.

---
+++ Portafolio/Servicios/RepositorioProyecto.cs
@@ -0,0 +1,50 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerProyectos();
+    }
+    public class RepositorioProyecto: IRepositorioProyectos
+    {
+
+      
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>() {
+                new Proyecto()
+            {
+                Titulo = "Metodo Consultores",
+                Descripcion = "Desarrollador Full-Stack para el Sistema Web de la Empresa, Implementado en Laravel.",
+                Link = "https://www.metodoconsultores.cl",
+                ImagenUrl = "/imagenes/Metodo.png",
+            },
+                new Proyecto()
+            {
+                Titulo = "Coasin Logicalis",
+                Descripcion = "Soporte Tecnico Terreno N1, Encargargado de los equipos tecnologicos de Codelco division RT.",
+                Link = "https://www.codelco.com/",
+                ImagenUrl = "/Imagenes/Codelco-RT.png",
+            },
+                new Proyecto()
+            {
+                Titulo = "Vantaz Group",
+                Descripcion = "Consultor Junior en Proyectos Tecnológicos para la Minera BHP.",
+                Link = "https://vantaz.com/",
+                ImagenUrl = "/Imagenes/Vantaz.png",
+            },
+                new Proyecto()
+            {
+                Titulo = "PcFactory",
+                Descripcion = "Soporte Técnico a Medio Tiempo, Responsable de la Revisión y Reparación de Productos Adquiridos en la Tienda.",
+                Link = "https://www.pcfactory.cl/",
+                ImagenUrl = "/Imagenes/pcFactory.png",
+            }
+
+            };
+        }
+    }
+
+
+}
